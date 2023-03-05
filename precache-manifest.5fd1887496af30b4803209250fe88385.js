9fe4070877e825dfd6313ec75e20a9652d4f5dd5
Abderrazak Mahi <abderrazakmahiii@gmail.com>
2023-03-05 14:39:18 +0000

    Updates

---
+++ precache-manifest.5fd1887496af30b4803209250fe88385.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0fda76b615d4c39f8ca",
+    "url": "/myportfolio/static/css/main.c599b118.chunk.css"
+  },
+  {
+    "revision": "c0fda76b615d4c39f8ca",
+    "url": "/myportfolio/static/js/main.c0fda76b.chunk.js"
+  },
+  {
+    "revision": "3bc8ac50b02e373cc067",
+    "url": "/myportfolio/static/js/1.3bc8ac50.chunk.js"
+  },
+  {
+    "revision": "8d284c95ee0deeab8dc5",
+    "url": "/myportfolio/static/css/2.2d6ef607.chunk.css"
+  },
+  {
+    "revision": "8d284c95ee0deeab8dc5",
+    "url": "/myportfolio/static/js/2.8d284c95.chunk.js"
+  },
+  {
+    "revision": "4f0a4cd72164fb940b23",
+    "url": "/myportfolio/static/js/runtime~main.4f0a4cd7.js"
+  },
+  {
+    "revision": "c1d0470316c110d7299e8a0f853f5f52",
+    "url": "/myportfolio/static/media/home.c1d04703.jpg"
+  },
+  {
+    "revision": "1c89de51ff7ddabeec6e357e8343efd1",
+    "url": "/myportfolio/static/media/contact.1c89de51.jpg"
+  },
+  {
+    "revision": "aa368007bf3c505e8e99c0de3878e477",
+    "url": "/myportfolio/static/media/frontend.aa368007.png"
+  },
+  {
+    "revision": "5ae3ed0df7d32b4d68dcfa5a495b9ef6",
+    "url": "/myportfolio/static/media/ai.5ae3ed0d.png"
+  },
+  {
+    "revision": "064ee3af0456d2438603eb24aea6907b",
+    "url": "/myportfolio/static/media/tiktok.064ee3af.png"
+  },
+  {
+    "revision": "74171f66b86f04705c25b448d761de2b",
+    "url": "/myportfolio/static/media/ms.74171f66.png"
+  },
+  {
+    "revision": "6cef2d0f25666c549f18a2d96e3fad2e",
+    "url": "/myportfolio/static/media/tm.6cef2d0f.png"
+  },
+  {
+    "revision": "aa47809e6be9371c3f708899a7794841",
+    "url": "/myportfolio/static/media/coding.aa47809e.png"
+  },
+  {
+    "revision": "83000884843a6dce381eb5714c086d39",
+    "url": "/myportfolio/static/media/about.83000884.jpg"
+  },
+  {
+    "revision": "9b3356fae8a282de516ce40fa0d81954",
+    "url": "/myportfolio/index.html"
+  }
+];
