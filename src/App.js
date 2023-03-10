fe40a4dad42bb500255d8ded3a1ef498a73c1a38
Abderrazak Mahi <abderrazakmahiii@gmail.com>
2023-03-10 16:56:19 +0000

    just to see

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from './Components/navbar';
 import Sidebar from './Components/sidebar';
 import Home from './Components/home';
@@ -9,6 +9,15 @@ import Footer from './Components/footer';
 import './index.css';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://github.com/abderrazakmahiii/myportfolio/blob/main/src/data.json')
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.log(error));
+  }, []);
+
   return (
     <React.Fragment>
       <Navbar />
